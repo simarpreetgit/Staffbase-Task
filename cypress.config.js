a1fad8baf72651343d27e56ed1da8d170b3b4dd7
Simarpreet <preet091989@gmail.com>
2022-12-04 14:32:07 +0100

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: 'cypress/e2e/**/*.{js,jsx}',
+    chromeWebSecurity: false,
+  },
+})
