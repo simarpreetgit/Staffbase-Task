4ee51137ef2cb8dddca1b96547857032f371734a
Simarpreet <preet091989@gmail.com>
2022-12-06 11:47:22 +0100

    task done

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: 'cypress/e2e/**/*.{js,jsx}',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts}',
     chromeWebSecurity: false,
     experimentalSessionAndOrigin: true,
   },
